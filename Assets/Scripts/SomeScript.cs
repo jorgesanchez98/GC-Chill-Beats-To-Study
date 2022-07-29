719517913c3e7d0d58e1b3fdaf75e54ff0338217
Jorge Sánchez Arreola <A01209929@itesm.mx>
2022-07-28 22:20:16 -0500

    Movement added

--- Assets/Scripts/SomeScript.cs
+++ Assets/Scripts/SomeScript.cs
@@ -5,14 +5,29 @@ using UnityEngine;
 public class SomeScript : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    	void Start()
     {
-        
+    		transform.position = new Vector3(-20,0,-30);    
     }
 
     // Update is called once per frame
-    void Update()
+    	void Update()
     {
-        
+    		transform.position = transform.position + new Vector3(0, 0, 0.1f);
+		//Debug.Log("Hola");
     }
+
+	void OnCollisionEnter(Collision collision)
+    {
+		if (collision.gameObject.name == "Restart")
+		{
+			transform.position = new Vector3(-20,0,-30);    
+		}
+    }
+
+	void OnTriggerEnter(Collider collision)
+    {
+        Debug.Log("Trigger enter");
+    }
+
 }
