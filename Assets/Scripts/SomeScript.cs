8be0b2eb151e8c8d3bcb857aeafd4e15010564ae
Jorge Sánchez Arreola <42280945+jorgesanchez98@users.noreply.github.com>
2022-07-28 22:53:27 -0500

    Update SomeScript.cs

--- Assets/Scripts/SomeScript.cs
+++ Assets/Scripts/SomeScript.cs
@@ -5,18 +5,20 @@ using UnityEngine;
 public class SomeScript : MonoBehaviour
 {
     // Start is called before the first frame update
+    // Inicializa el objeto en una posiciòn predeterminada
     	void Start()
     {
     		transform.position = new Vector3(-20,0,-30);    
     }
 
     // Update is called once per frame
+    // Avanza el objeto en el eje Z
     	void Update()
     {
     		transform.position = transform.position + new Vector3(0, 0, 0.1f);
 		//Debug.Log("Hola");
     }
-
+    // Cuando choca con un objeto llamado "Restart", lleva el objeto de regreso a su posiciòn inicial
 	void OnCollisionEnter(Collision collision)
     {
 		if (collision.gameObject.name == "Restart")
